1172c2df259f9cf2dcf5fd18ae015fa95afbfbf8
HILLARY MUGASIA <168737611+Hilla22@users.noreply.github.com>
2024-06-14 14:15:59 +0300

    Add files via upload

---
+++ basisc.js
@@ -0,0 +1,11 @@
+console.log("hello world im a developer")
+let num1=50
+let num2=50.4
+
+let stng1="Nairobi"
+let stng2='eldoret'
+
+console.log(num1)
+console.log(num2)
+console.log(stng1)
+console.log(stng2)
